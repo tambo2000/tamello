bcf94f4afeb71c489bc6338b1cacb42904978165
Tam <tambo2000@gmail.com>
2013-09-02 11:16:57 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tamello::Application.config.secret_token = '2d25d101eaa12b5ccb3dd0df8e29f87b36a3656947efa683447aaafbce31c3b62b5c0e45d98160d99c12a7587798648ab79797a0ff57b052015edc709ebf026e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tamello::Application.config.session_store :cookie_store, key: '_tamello_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tamello::Application.config.session_store :active_record_store
